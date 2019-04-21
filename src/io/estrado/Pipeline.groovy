724a6c25a37df3d0c66eae7ea23a344d7c2fd062
Oliver Veits <oliver.veits@gmail.com>
2019-04-21 12:46:00 +0200

    cleaning

--- src/io/estrado/Pipeline.groovy
+++ src/io/estrado/Pipeline.groovy
@@ -93,25 +93,21 @@ def containerBuildPub(Map args) {
 
     println "Running Docker build/publish: ${args.host}/${args.acct}/${args.repo}:${args.tags}"
 
-    //docker.withRegistry("https://${args.host}", "${args.auth_id}") {
+    // perform docker login to container registry as the docker-pipeline-plugin doesn't work with the next auth json format
+    withCredentials([[$class          : 'UsernamePasswordMultiBinding', credentialsId: args.auth_id,
+            usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
+      sh "echo ${env.PASSWORD} | docker login -u ${env.USERNAME} --password-stdin ${args.host}"
+    }
 
-        withCredentials([[$class          : 'UsernamePasswordMultiBinding', credentialsId: args.auth_id,
-                usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
-          sh "echo ${env.PASSWORD} | docker login -u ${env.USERNAME} --password-stdin ${args.host}"
-        }
+    def img = docker.image("${args.host}/${args.acct}/${args.repo}")
 
-        def img = docker.image("${args.host}/${args.acct}/${args.repo}")
+    sh "docker build --build-arg VCS_REF=${env.GIT_SHA} --build-arg BUILD_DATE=`date -u +'%Y-%m-%dT%H:%M:%SZ'` -t ${args.host}/${args.acct}/${args.repo} ${args.dockerfile}"
 
-        sh "docker build --build-arg VCS_REF=${env.GIT_SHA} --build-arg BUILD_DATE=`date -u +'%Y-%m-%dT%H:%M:%SZ'` -t ${args.host}/${args.acct}/${args.repo} ${args.dockerfile}"
+    for (int i = 0; i < args.tags.size(); i++) {
+        img.push(args.tags.get(i))
+    }
 
-        for (int i = 0; i < args.tags.size(); i++) {
-            //sh "docker tag ${args.host}/${args.acct}/${args.repo} ${args.host}/${args.acct}/${args.repo}:${args.tags.get(i)}"
-            //sh "docker push ${args.host}/${args.acct}/${args.repo}:${args.tags.get(i)}"
-            img.push(args.tags.get(i))
-        }
-
-        return img.id
-    //}
+    return img.id
 }
 
 def getContainerTags(config, Map tags = [:]) {
