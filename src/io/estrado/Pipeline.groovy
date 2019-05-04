45ff77daa2d8405cc80b4793199a99762d4a5db9
Oliver Veits <oliver.veits@web.de>
2019-05-04 17:23:30 +0200

    helm purge deleted version to avoid Error: UPGRADE FAILED: "..." has no deployed releases

--- src/io/estrado/Pipeline.groovy
+++ src/io/estrado/Pipeline.groovy
@@ -58,7 +58,7 @@ def helmDeploy(Map args) {
 def helmDelete(Map args) {
         println "Running helm delete ${args.name}"
 
-        sh "helm delete ${args.name}"
+        sh "helm delete ${args.name} --purge"
 }
 
 def helmTest(Map args) {
