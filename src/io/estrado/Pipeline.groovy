9cd695e3c57f47bad8291aff71b488be338d8be9
Oliver Veits <oliver.veits@gmail.com>
2019-06-04 08:52:37 +0200

    added missing def String release_overrides_string = ""

--- src/io/estrado/Pipeline.groovy
+++ src/io/estrado/Pipeline.groovy
@@ -117,6 +117,10 @@ def helmDeploy(Map args) {
     if (args.set) {
       release_overrides = getHelmReleaseOverrides(args.set)
     }
+
+    // we had a problem with commit.sha values that have no alphabetic character (e.g. commit.sha=8880525 was translated to 8.880525e+06) 
+    // this is fixed in https://github.com/helm/helm/issues/1707 by introducing the --set-string option.
+    def String release_overrides_string = ""
     if (args.set-string) {
       release_overrides_string = getHelmReleaseOverrides(args.set-string)
     }
