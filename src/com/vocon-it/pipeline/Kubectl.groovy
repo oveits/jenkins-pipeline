7d6cb11642dc8e3d9374adf1e760e5a720564cdb
Oliver Veits <oliver.veits@gmail.com>
2019-06-17 23:44:26 +0200

    renamed Kube.groovy to Kubectl.groovy

---
+++ src/com/vocon-it/pipeline/Kubectl.groovy
@@ -0,0 +1,17 @@
+#!/usr/bin/groovy
+package com.vocon-it.pipeline;
+
+def getNodes() {
+    // Test that kubectl can correctly communication with the Kubernetes API
+    println "checking kubectl connnectivity to the API"
+    sh "kubectl get nodes"
+
+}
+
+def getAll(String namespace, Boolean ignoreErrors) {
+    if(ignoreErrors || ignoreErrors == null) {
+        sh "kubectl -n ${namespace} get all || true"
+    } else {
+        sh "kubectl -n ${namespace} get all"
+    }  
+}
